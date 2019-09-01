8c9fb6dc35164af27b59d6a8918a89656a33824e
apple <apple@192.168.0.105>
2019-09-01 15:05:09 +0530

    update

---
+++ AllAboutRest/src/main/java/com/example/rest/api/example/controllers/TestController.java
@@ -0,0 +1,18 @@
+package com.example.rest.api.example.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+	@GetMapping("/message")
+	public ResponseEntity<String> getMessage() throws InterruptedException {
+		Thread.sleep(4000);
+		return ResponseEntity.badRequest().body("Error happened");
+	}
+
+}
