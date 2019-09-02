68611f9445ec565877778ab38bf07227c6e03f0a
apple <apple@192.168.0.104>
2019-09-02 23:13:00 +0530

    update

--- AllAboutRest/src/main/java/com/example/rest/api/controller/TopicController.java
+++ AllAboutRest/src/main/java/com/example/rest/api/controller/TopicController.java
@@ -1,7 +1,6 @@
 package com.example.rest.api.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.rest.api.exception.BadRequestException;
