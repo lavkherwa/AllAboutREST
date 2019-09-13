9d5d51fd970227aa8383ec6144b525af7ed315fe
apple <apple@172.20.10.12>
2019-09-13 21:49:21 +0530

    Handling static image/pdf resources

--- AllAboutRest/src/main/java/com/example/rest/api/controller/TestController.java
+++ AllAboutRest/src/main/java/com/example/rest/api/controller/TestController.java
@@ -1,8 +1,14 @@
 package com.example.rest.api.controller;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +19,18 @@ public class TestController {
 	public ResponseEntity<String> getMessage() throws InterruptedException {
 
 		return ResponseEntity.ok().body("Hello Everyone!!");
+	}
+
+	@GetMapping(value = "/image", produces = MediaType.IMAGE_JPEG_VALUE)
+	public @ResponseBody byte[] getImage() throws IOException {
+		InputStream in = getClass().getResourceAsStream("/static/image.png");
+		return IOUtils.toByteArray(in);
+	}
+	
+	@GetMapping(value = "/pdf", produces = MediaType.APPLICATION_PDF_VALUE)
+	public @ResponseBody byte[] getPdf() throws IOException {
+		InputStream in = getClass().getResourceAsStream("/static/sample.pdf");
+		return IOUtils.toByteArray(in);
 	}
 
 }
