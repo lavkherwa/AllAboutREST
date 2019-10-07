9acc16fc74cb7412f617a8556584ff27c4e7a887
apple <apple@192.168.0.100>
2019-10-07 14:08:38 +0530

    Added StreamingResponseBody example for returning stream data

--- AllAboutRest/src/main/java/com/example/rest/api/controller/TestController.java
+++ AllAboutRest/src/main/java/com/example/rest/api/controller/TestController.java
@@ -4,12 +4,14 @@ import java.io.IOException;
 import java.io.InputStream;
 
 import org.apache.commons.io.IOUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 @RestController
 @RequestMapping("/api")
@@ -26,11 +28,23 @@ public class TestController {
 		InputStream in = getClass().getResourceAsStream("/static/image.png");
 		return IOUtils.toByteArray(in);
 	}
-	
+
 	@GetMapping(value = "/pdf", produces = MediaType.APPLICATION_PDF_VALUE)
 	public @ResponseBody byte[] getPdf() throws IOException {
 		InputStream in = getClass().getResourceAsStream("/static/sample.pdf");
 		return IOUtils.toByteArray(in);
+	}
+
+	@GetMapping(value = "/pdfStream", produces = MediaType.APPLICATION_PDF_VALUE)
+	public ResponseEntity<StreamingResponseBody> getPdfStream() throws IOException {
+
+		StreamingResponseBody stream = out -> {
+			InputStream in = getClass().getResourceAsStream("/static/sample.pdf");
+			out.write(IOUtils.toByteArray(in));
+		};
+
+		return new ResponseEntity<StreamingResponseBody>(stream, HttpStatus.OK);
+
 	}
 
 }
